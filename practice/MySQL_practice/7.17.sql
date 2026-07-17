e4b0db8f4f475dab50ddb3fc048e85e91f4461dc
qkf12 <3273892007@qq.com>
2026-07-17 21:20:36 +0800

    学习了基础的sql增删改查

---
+++ practice/MySQL_practice/7.17.sql
@@ -0,0 +1,14 @@
+-- 查询数据 *表示所有值
+-- select * from c;
+-- select id,age from c;
+-- select name,id,age from c;
+
+-- 修改数据
+-- update c set age = 30 where id = 1;
+-- update c set name = '张三' where name = '曹操';
+
+-- update c set age = age + 1 where id > 3;
+
+-- 删除数值
+-- delete from c where id = 5;
+truncate table c;
