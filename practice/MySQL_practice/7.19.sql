dc29c5c543096d815921cb2481d1307e568ff89e
qkf12 <3273892007@qq.com>
2026-07-18 21:47:58 +0800

    学习了一点约束

---
+++ practice/MySQL_practice/7.19.sql
@@ -0,0 +1,10 @@
+--列名 数据类型 约束（主键 primary key 非空 not null  自增长 auto_increment） 
+--  create table a ( 
+--    id  int primary key auto_increment ,
+--  name varchar (20));
+
+-- 删除整个表格
+-- drop table  if exists a ;
+ 
+--  insert into a values(0,'张三');
+
