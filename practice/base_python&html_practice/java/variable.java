1a085ccf4bb32f6f06631d1ced2a535939945c9a
qkf12 <3273892007@qq.com>
2026-07-20 20:48:23 +0800

    字面量和变量

---
+++ practice/base_python&html_practice/java/variable.java
@@ -0,0 +1,16 @@
+package com.qkf12.practice;
+
+public class variable {
+    public static void main(String[] args){
+        double a = 0;
+        double b = 10;
+        double c = 20;
+        double sum = (a + b + c);
+        System.out.println(sum);
+        a += 10;
+
+        a -=2;
+        System.out.println(a);
+
+    }
+}
