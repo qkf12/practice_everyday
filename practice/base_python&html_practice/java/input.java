4416af1368f42e7f4d368cdd2a4dc57ea4bc4c84
qkf12 <3273892007@qq.com>
2026-07-21 20:35:57 +0800

    基本的java逻辑运算符和语法

---
+++ practice/base_python&html_practice/java/input.java
@@ -0,0 +1,20 @@
+package com.qkf12.practice;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+//          输入整数
+//        int num = sc.nextInt();
+//        System.out.println(num);
+
+//        输入两位小数
+//        double num = sc.nextDouble();
+//        System.out.println(num);
+
+          String num = sc.next();
+          System.out.println(num);
+    }
+}
---
+++ practice/base_python&html_practice/java/upper_lower.java
@@ -0,0 +1,10 @@
+package com.qkf12.practice;
+
+public class upper_lower {
+    public static void main(String[] args){
+        char  c = 'V';
+        char  cc = (char)(c + 32);
+        System.out.println(cc);
+
+    }
+}
