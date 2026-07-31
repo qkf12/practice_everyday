e8c2c5e1fb65f190b5ef14c0238c530d1cecae6e
qkf12 <3273892007@qq.com>
2026-07-31 23:43:02 +0800

    string的简单使用

---
+++ practice/base_language_practice/java/7.31/practice_string.java
@@ -0,0 +1,28 @@
+package com.qkf12.practice;
+
+public class practice_string {
+    static void main() {
+//        String s = "黄晓明,王祖蓝,鹿晗,张良";
+//        String[] s1 = s.split(",");
+//        for (int i = 0; i < s1.length; i++) {
+//            System.out.println(s1[i]);
+//        }
+        String s =  "黄晓明";
+//        char[] arr = s.toCharArray();
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+//        String s1 = s.replace("黄","刘");
+//        System.out.println(s1);
+
+//        substring 包前不包后
+//        System.out.println(s.substring(0,2));
+
+//        System.out.println(s.startsWith("张"));
+//        System.out.println(s.startsWith("黄"));
+
+
+
+    }
+
+}
