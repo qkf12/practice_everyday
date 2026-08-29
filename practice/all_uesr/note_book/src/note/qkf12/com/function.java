5b5311975cb6522909435a7f6171bf625104b3a9
qkf12 <3273892007@qq.com>
2026-08-29 21:37:30 +0800

    记事本项目 基本的增删改查完成了 明天连接数据库 后天连接前端

--- practice/all_uesr/note_book/src/note/qkf12/com/function.java
+++ practice/all_uesr/note_book/src/note/qkf12/com/function.java
@@ -1,5 +1,6 @@
 package note.qkf12.com;
 
+import java.awt.print.Printable;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,7 +8,7 @@ import java.util.Scanner;
 
 public class function {
     public static Scanner sc ;
-    public static ArrayList list = new ArrayList();
+    public static ArrayList<nothing> list = new ArrayList<>();
 
 
 //    ======================主系统==================
@@ -27,10 +28,10 @@ public class function {
                     add_function();
                     break;
                 case "2":
-//                    delete_function();
+                    delete_function();
                     break;
                 case "3":
-//                    update_function();
+                    update_function();
                     break;
                 case "4":
                     check_function();
@@ -41,6 +42,7 @@ public class function {
             }
         }
     }
+    //=================================添加新信息====================
     public static void add_function(){
         sc = new Scanner(System.in);
 
@@ -61,8 +63,78 @@ public class function {
         }
 
     }
-    public static void check_function(){
-        System.out.println(list);
+    //======================删除信息=========================
+    public static void delete_function(){
+        sc = new Scanner(System.in);
+        System.out.println("请输入要删除的日期");
+        String answer = sc.next();
+        for (int i = list.size() -1; i >= 0 ; i--) {
+            nothing note = list.get(i);
+            if(note.getDate().equals(answer)){
+                String date = note.getDate();
+                String type = note.getType();
+                double money = note.getMoney();
+                System.out.println("您确定要删除这个吗  是/否");
+                System.out.println("日期\t类型\t金额");
+                System.out.println("==========================");
+                System.out.printf("%s\t%s\t%.2f\n",date, type, money);
+                String answer1 = sc.next();
+                if(answer1.equals("是")){
+                    System.out.println("====删除成功====");
+                    list.remove(i);
+                }
+            }
 
+
+        }
+    }
+
+
+
+    //===========================更改信息======================
+    public static void update_function(){
+        sc = new Scanner(System.in);
+        System.out.println("请输入你想修改信息的日期");
+        String date = sc.next();
+        for (int i = 0; i < list.size(); i++) {
+            nothing note = list.get(i);
+            if(note.getDate().equals(date)){
+                String date1 = note.getDate();
+                String type = note.getType();
+                double money = note.getMoney();
+                System.out.println("您要修改哪一列呀");
+                System.out.println("======================");
+                System.out.println("日期\t类型\t金额");
+                System.out.printf("%s\t%s\t%.2f\n",date1,type,money);
+                String date3 = sc.next();
+                if(date3.equals("类型")){
+                    System.out.println("请输入正确的 <类型>");
+                    String type3 = sc.next();
+                    note.setType(type3);
+                    System.out.println("修改<类型>成功");
+                }
+                if (date3.equals("金额")){
+                    System.out.println("请输入正确的 <金额>");
+                    Double money3 = sc.nextDouble();
+                    note.setMoney(money3);
+                    System.out.println("修改<金额>成功");
+                }
+                return;
+            }
+        }
+
+    }
+    //=============================查看所有信息======================
+    public static void check_function(){
+        System.out.println("日期\t类型\t金额");
+        System.out.println("======================");
+//        System.out.println(list);/*测试 类里的@Override*/
+        for (int i = 0; i < list.size(); i++) {
+            nothing note = list.get(i);
+            String date = note.getDate();
+            String type = note.getType();
+            double money = note.getMoney();
+            System.out.printf("%s\t%s\t%.2f\n",date,type,money);
+        }
     }
 }
