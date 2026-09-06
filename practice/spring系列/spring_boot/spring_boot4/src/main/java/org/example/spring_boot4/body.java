f9c1044ef56c8b0a76d919ffc40b154f9b208822
qkf12 <3273892007@qq.com>
2026-09-06 23:05:45 +0800

    用value和environment的方法打印

--- practice/spring系列/spring_boot/spring_boot4/src/main/java/org/example/spring_boot4/body.java
+++ practice/spring系列/spring_boot/spring_boot4/src/main/java/org/example/spring_boot4/body.java
@@ -1,7 +1,9 @@
 package org.example.spring_boot4;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +15,28 @@ public class body {
     @Value("${name}")
     private String name;
 
+    @Value("${person.age}")
+    private int age;
+
+    @Value("${arr[0]}")
+    private String arr1;
+
+    @Autowired
+    private Environment env;
+
+
+
 
     @RequestMapping("/hello")
     public String hello() {
         System.out.println(name);
+        System.out.println(age);
+        System.out.println("=================");
+        System.out.println(env.getProperty("person.name"));
+        System.out.println(env.getProperty("person.age"));
+        System.out.println("=================");
+        System.out.println(arr1);
+
         return "hello world";
     }
 }
