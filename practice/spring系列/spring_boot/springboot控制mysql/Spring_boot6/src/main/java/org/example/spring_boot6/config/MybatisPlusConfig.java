e8ca008dfb4f4466638d9b8304a2493dd13c0b38
qkf12 <3273892007@qq.com>
2026-09-16 23:12:38 +0800

    springboot6 添加了涉及的知识点 并且综合知识点 也有所更改其错误

--- practice/spring系列/spring_boot/springboot控制mysql/Spring_boot6/src/main/java/org/example/spring_boot6/config/MybatisPlusConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.spring_boot6.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@MapperScan("org.example.spring_boot6.mapper")
-@Configuration
-public class MybatisPlusConfig {
-}
